6b472a5d4fba7d0e55dd6df539cb73c228d9fa0f
Eagle19243 <eagle19243@gmail.com>
2019-08-12 23:59:31 -0400

    add checkbox to allow data to be overwritten

--- public/js/field-settings.js
+++ public/js/field-settings.js
@@ -3,6 +3,12 @@ window.onload = () => {
 }
 
 function init() {
+    if (isSource()) {
+        $('.container-overwrite').hide();
+    } else {
+        $('.container-overwrite').show();
+    }
+
     $('.btn-save').click(saveButtonClicked);
 }
 
